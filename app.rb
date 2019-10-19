08b1e63cb479a65b3c88180ad61daeedbb4b8b25
Orlando De La Garza <OrlandoJDLG@gmail.com>
2019-10-19 17:44:23 -0500

    Initial Commit

---
+++ app.rb
@@ -0,0 +1,171 @@
+require "sinatra"
+require 'sinatra/flash'
+require_relative "authentication.rb"
+require_relative "api_authentication.rb"
+#the following urls are included in authentication.rb
+# GET /login
+# GET /logout
+# GET /sign_up
+
+# authenticate! will make sure that the user is signed in, if they are not they will be redirected to the login page
+# if the user is signed in, current_user will refer to the signed in user object.
+# if they are not signed in, current_user will be nil
+
+get "/" do
+	erb :index
+end
+
+
+get "/dashboard" do
+	authenticate!
+	erb :dashboard
+end
+
+#USER STUFF////////////////////////////
+get "/users" do
+	#halt 200, Book.all.to_json
+	halt 200, User.all.to_json(except: [:password])
+end
+
+get "/users/:id" do
+	id = params["id"]
+	u = User.get(id)
+	if u != nil
+		halt 200, u.to_json(except: [:password])
+	else
+		halt 404, {message: "User not found"}.to_json
+	end
+end
+
+patch "/users/:id" do
+	#halt 200, Book.all.to_json
+	halt 200, {message: "Not implemented"}.to_json
+end
+
+delete "/users/:id" do
+	id = params["id"]
+	u = User.get(id)
+	if u != nil
+		u.destroy
+		halt 200, {message: "User deleted"}.to_json
+	else
+		halt 404, {message: "User not found"}.to_json
+	end
+end
+
+#BOOKS STUFF////////////////////////////////////////
+post "/books" do
+	#halt 200, Book.all.to_json
+	halt 200, {message: "Not implemented"}.to_json
+end
+
+get "/books" do
+	#halt 200, Book.all.to_json
+	halt 200, {message: "Not implemented"}.to_json
+end
+
+get "/books/check_out" do
+	#get all books checked out that have not been returned
+	#make an empty array of books
+	#loop through checkouts and add each book to the array
+	#return the array as json
+	books = []
+	checkouts = Check_Out.all(returned: false)
+	checkouts.each do |c|
+		books << c.book
+	end
+	halt 200, books.to_json
+end
+
+get "/books/check_in" do
+	#halt 200, Book.all.to_json
+	halt 200, {message: "Not implemented"}.to_json
+end
+
+get "/books/:id" do
+	# id = params["id"]
+	# b = Book.get(id)
+	# if b != nil
+	# 	halt 200, b.to_json
+	# else
+	# 	halt 404, {message: "Book not found"}.to_json
+	# end
+end
+
+patch "/books/:id" do
+	#halt 200, Book.all.to_json
+	halt 200, {message: "Not implemented"}.to_json
+end
+
+delete "/books/:id" do
+	#halt 200, Book.all.to_json
+	halt 200, {message: "Not implemented"}.to_json
+
+end
+
+#CUSTOMERS STUFF////////////////////////////
+post "/customers" do
+	#halt 200, Book.all.to_json
+	halt 200, {message: "Not implemented"}.to_json
+end
+
+get "/customers" do
+	#halt 200, Book.all.to_json
+	halt 200, {message: "Not implemented"}.to_json
+end
+
+get "/customers/:id" do
+	# id = params["id"]
+	# b = Book.get(id)
+	# if b != nil
+	# 	halt 200, b.to_json
+	# else
+	# 	halt 404, {message: "Book not found"}.to_json
+	# end
+end
+
+patch "/customers/:id" do
+	#halt 200, Book.all.to_json
+	halt 200, {message: "Not implemented"}.to_json
+end
+
+delete "/customers/:id" do
+	#halt 200, Book.all.to_json
+	halt 200, {message: "Not implemented"}.to_json
+end
+
+#CHECK_OUT STUFF////////////////////////////
+post "/check_outs" do
+	#halt 200, Book.all.to_json
+	halt 200, {message: "Not implemented"}.to_json
+end
+
+get "/check_outs" do
+	#halt 200, Book.all.to_json
+	halt 200, {message: "Not implemented"}.to_json
+end
+
+get "/check_outs/not_returned" do
+	#halt 200, Book.all.to_json
+	halt 200, {message: "Not implemented"}.to_json
+end
+
+get "/check_outs/:id_c/:id_b"  do#       :id" do #ASK??
+	# id = params["id"]
+	# b = Book.get(id)
+	# if b != nil
+	# 	halt 200, b.to_json
+	# else
+	# 	halt 404, {message: "Book not found"}.to_json
+	# end
+end
+
+patch "/check_outs/:id" do
+	#halt 200, Book.all.to_json
+	halt 200, {message: "Not implemented"}.to_json
+end
+
+delete "/check_outs/:id" do
+	#halt 200, Book.all.to_json
+	halt 200, {message: "Not implemented"}.to_json
+end
---
+++ models.rb
@@ -0,0 +1,94 @@
+require 'data_mapper' # metagem, requires common plugins too.
+
+# need install dm-sqlite-adapter
+# if on heroku, use Postgres database
+# if not use sqlite3 database I gave you
+if ENV['DATABASE_URL']
+  DataMapper::setup(:default, ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
+else
+  DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/app.db")
+end
+
+class User
+    include DataMapper::Resource
+    property :id, Serial
+    property :email, Text
+    property :password, Text
+    property :role_id, Integer
+    property :fname, Text
+    property :lname, Text
+    property :phone_number, Text
+    property :created_at, DateTime
+
+    def login(password)
+        return self.password == password
+    end
+end
+
+class Book
+    include DataMapper::Resource
+    property :id, Serial
+    property :title, Text
+    property :edition, Text
+    property :author, Integer
+    property :isbn, Text
+    property :description, Text
+    property :checked_out, Boolean
+    property :created_at, DateTime
+
+    def current_check_out
+        return Check_Out.first(book_id: id, returned: false)
+    end
+
+    def currently_checked_out?
+        co = Check_Out.first(book_id: id, returned: false)
+        return !co.nil?
+    end
+end
+
+class Customer
+    include DataMapper::Resource
+    property :id, Serial
+    property :fname, Text
+    property :lname, Text
+    property :phone_number, Integer
+    property :created_at, DateTime
+
+    def check_outs
+        return Check_Out.all(customer_id: id)
+    end
+
+    def unreturned_check_outs
+        return Check_Out.all(customer_id: id, returned: false)
+    end
+end
+
+class Check_Out
+    include DataMapper::Resource
+    property :id, Serial
+    property :customer_id, Integer
+    property :book_id, Integer
+    property :due_date, Text
+    property :returned, Boolean
+    property :checked_out_date, Boolean
+    property :created_at, DateTime
+
+    def customer
+        return Customer.get(customer_id)
+    end
+
+    def book
+        return Book.get(book_id)
+    end
+end
+
+# Perform basic sanity checks and initialize all relationships
+# Call this when you've defined all your models
+DataMapper.finalize
+
+# automatically create the post table
+User.auto_upgrade!
+Book.auto_upgrade!
+Customer.auto_upgrade!
+Check_Out.auto_upgrade!
+
